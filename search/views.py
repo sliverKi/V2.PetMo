3f1e73b73ee290828a9b644bd9c640d994e27a01
sliverKi <lovee2756@gmail.com>
2023-10-14 16:28:08 +0900

    Feat: success connection Elasticsearch-Kibana

--- search/views.py
+++ search/views.py
@@ -4,15 +4,28 @@ from rest_framework.pagination import PageNumberPagination
 from posts.serializers import PostSearchSerializer
 from .documents import PostDocument
 from elasticsearch_dsl import Q
-
+from datetime import datetime
+from elasticsearch import Elasticsearch
 
 class SearchPost(APIView, PageNumberPagination):
     post_serializer=PostSearchSerializer
     search_document=PostDocument
+
+    def log_search_query(self, query):
+        es = Elasticsearch([{'host': 'elasticsearch', 'port': 9200}])
+        log_body = {
+            "query": query,
+            "timestamp": datetime.utcnow()
+        }
+        es.index(index="search_logs", doc_type="_doc", body=log_body)
+
    
     def get(self, request, query=None):
         try:
+            
+            self.log_search_query(query)
             print(query)
+
             q=Q(
                 "multi_match",
                 query=query,
@@ -30,3 +43,5 @@ class SearchPost(APIView, PageNumberPagination):
 
         except Exception as e:
             return HttpResponse(str(e), status=500)
+        
+    
